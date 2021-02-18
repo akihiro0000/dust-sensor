f0ed4bc07fe8e0df661667488982c1b9b75b3829
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-02-19 00:25:04 +0900

    Update run.py

--- run.py
+++ run.py
@@ -40,9 +40,9 @@ def get_pm25(PIN):
             print(pcs2ugm3(concent), " [ug/m^3]")
             print("-------------------")
             tim = '"timestamp":"'+datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')+'"'
-            rate = 'ratio[%]:'+str(ratio)
-            con = 'concent[pcs/0.01cf]:' + str(concent)
-            pcs2 = 'pcs2ugm3[ug/m^3]:' + str(pcs2ugm3(concent))
+            rate = '"ratio[%]":'+'"'+str(ratio)+'"'
+            con = '"concent[pcs/0.01cf]":' +'"'+str(concent)+'"'
+            pcs2 = '"pcs2ugm3[ug/m^3]":' + '"' +str(pcs2ugm3(concent))+'"'
             jsontext = "{"+tim+","+rate+","+con+","+pcs2+"}"
             mqtt_client.publish("{}/{}".format("/demo",'car_count'), jsontext)
             break
